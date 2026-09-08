58649f9bc8034691f0e44d6d893c66d58f98a389
wwinkqxx <mimuzzaa@gmail.com>
2026-09-08 21:08:01 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Fibonacci
+    {
+        public static int[] readNumbers(string path)
+        {
+            string[] numbers = File.ReadAllText(path).Split();
+            int[] ints = new int[numbers.Length];
+            for (int i=0; i<numbers.Length; i++)
+            {
+                ints[i] = int.Parse(numbers[i]);
+            }
+            return ints;
+        }
+        public static int readStep(string path)
+        {
+            string number = File.ReadAllText(path);
+            int step = int.Parse(number);
+            return step;
+        }
+        public static void writeFibonacci(string path, List<int> fib)
+        {
+            File.WriteAllText(path, string.Join(" ", fib));
+        }
+
+        // Source - https://stackoverflow.com/a/17634529
+        // Posted by Gus
+        // Retrieved 2026-09-06, License - CC BY-SA 3.0
+        public static void CalculateFibonacci(int a, int b, int counter, int step, List<int> result)
+        {
+            if (counter > step) return;
+
+            result.Add(a);
+            CalculateFibonacci(b, a + b, counter + 1, step, result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "C:\\Users\\Користувач\\source\\repos\\Fibonacci\\input.txt";
+            string input2 = "C:\\Users\\Користувач\\source\\repos\\Fibonacci\\step.txt";
+
+            int[] numbers = Fibonacci.readNumbers(input1);
+            int step = Fibonacci.readStep(input2);
+
+            List<int> fib = new List<int>();
+            Fibonacci.CalculateFibonacci(numbers[0], numbers[1], 1, step, fib);
+
+            string output = "C:\\Users\\Користувач\\source\\repos\\Fibonacci\\output.txt";
+            Fibonacci.writeFibonacci(output, fib);
+        }
+    }
+}
