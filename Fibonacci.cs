3b4e5fdec1b77228293a3326a12725b3345e3e18
wwinkqxx <mimuzzaa@gmail.com>
2026-09-09 10:20:14 +0300

    Refactor Fibonacci methods and add limitations
    Refactored Fibonacci calculation methods to use recursion and added limitations for results.

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,36 +8,21 @@ namespace Task1
 {
     class Fibonacci
     {
-        public static int[] readNumbers(string path)
+        public static List <int> CalculateFibonacci(int step, List<int> result)
         {
-            string[] numbers = File.ReadAllText(path).Split();
-            int[] ints = new int[numbers.Length];
-            for (int i=0; i<numbers.Length; i++)
-            {
-                ints[i] = int.Parse(numbers[i]);
-            }
-            return ints;
-        }
-        public static int readStep(string path)
-        {
-            string number = File.ReadAllText(path);
-            int step = int.Parse(number);
-            return step;
-        }
-        public static void writeFibonacci(string path, List<int> fib)
-        {
-            File.WriteAllText(path, string.Join(" ", fib));
+            if (step == 0) return result;
+
+            result.Add(result[^1] + result[^2]);
+            return CalculateFibonacci(step - 1, result);
         }
 
-        // Source - https://stackoverflow.com/a/17634529
-        // Posted by Gus
-        // Retrieved 2026-09-06, License - CC BY-SA 3.0
-        public static void CalculateFibonacci(int a, int b, int counter, int step, List<int> result)
+        public static List<int> CalculateFibonacciLimitations(int limit, List<int> result)
         {
-            if (counter > step) return;
+            if (result[^1] > limit) return result;
 
-            result.Add(a);
-            CalculateFibonacci(b, a + b, counter + 1, step, result);
+            result.Add(result[^1] + result[^2]);
+            return CalculateFibonacciLimitations(limit, result);
         }
+
     }
 }
