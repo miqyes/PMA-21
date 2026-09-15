dac2cb607e58f1ee8b79963d7d508808f1d4add6
vitatitska <vvvvitaa@gmail.com>
2026-09-15 21:43:04 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp4
+{
+    public class Fibonacci
+    {
+        public static List<int> Recursion(List<int> numbers, int steps)
+        {
+            if (steps == 0)
+            {
+                return numbers;
+            }
+            int nextNum = numbers[numbers.Count - 1] + numbers[numbers.Count - 2];
+            numbers.Add(nextNum);
+            return Recursion(numbers, steps - 1);
+
+        }
+        public static List<int> Limit(List<int> numbers, int lim)
+        {
+            while (numbers[numbers.Count - 1] > lim)
+            {
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+            return numbers;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleApp4;
+using System;
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            if (!File.Exists("input.txt"))
+            {
+                Console.WriteLine("Error: cannot find file input.txt");
+                return;
+            }
+
+            string[] numbers = File.ReadAllText("input.txt").Split(',');
+            if (numbers.Length < 2)
+            {
+                Console.WriteLine("Error: in file input.txt should be at least 2 numbers");
+                return;
+            }
+
+            int first = int.Parse(numbers[0]);
+            int second = int.Parse(numbers[1]);
+
+            if (!File.Exists("steps.txt"))
+            {
+                Console.WriteLine("Error: cannot find file steps.txt");
+                return;
+            }
+            int steps = int.Parse(File.ReadAllText("steps.txt"));
+
+            if (!File.Exists("limit.txt"))
+            {
+                Console.WriteLine("Error: cannot find file limit.txt");
+                return;
+
+            }
+            int lim = int.Parse(File.ReadAllText("limit.txt"));
+
+            List<int> list = new List<int>();
+
+            list.Add(first);
+            list.Add(second);
+
+            Fibonacci.Recursion(list, steps);
+            Fibonacci.Limit(list, lim);   
+
+            string result = string.Join(",", list);
+            File.WriteAllText("output.txt", result);
+            Console.WriteLine(result);
+
+        }
+    }
+}
