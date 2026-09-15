d797f72e2f603b2244d7d7e3a68c5aee1050ab93
Мицько Ярина <yaryna.myt@gmail.com>
2026-09-15 21:49:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace fibonachi
+{
+    class Program
+    {
+        public static void Main()
+        {
+            if (!File.Exists("input.txt") || !File.Exists("steps.txt"))
+            {
+                Console.WriteLine("Error: files are not found!");
+                return;
+            }
+
+            string input = File.ReadAllText("input.txt");
+            string steps = File.ReadAllText("steps.txt");
+
+            string[] numbers = input.Split(',');
+            if (numbers.Length != 2)
+            {
+                Console.WriteLine("Error: in file 'input.txt' must be 2 numbers!");
+                return;
+            }
+
+            int firstNum = int.Parse(numbers[0]);
+            int secondNum = int.Parse(numbers[1]);
+            int numOfSteps = int.Parse(steps);
+
+            if (firstNum < 0 || secondNum < 0 || numOfSteps < 0)
+            {
+                Console.WriteLine("Error: numbers can't be negative!");
+                return;
+            }
+            List<int> stepsList = new List<int> { firstNum, secondNum };
+            fibonachi.bySteps(stepsList, numOfSteps - 2);
+
+            Console.Write("Enter limit: ");
+            int limit = int.Parse(Console.ReadLine());
+
+            List<int> limitList = new List<int> { firstNum, secondNum };
+            fibonachi.byLimit(limitList, limit);
+
+            string stepsResult = "By steps: " + string.Join(", ", stepsList);
+            string limitResult = "By limit: " + string.Join(", ", limitList);
+
+            string result = stepsResult + Environment.NewLine + limitResult;
+
+            File.WriteAllText("result.txt", result);
+
+            Console.WriteLine("Result is successfully saved in file 'result.txt'");
+        }
+    }
+}
---
+++ fibonachi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fibonachi
+{
+    public static class fibonachi
+    {
+        public static List<int> bySteps(List<int> list, int count)
+        {
+            if (count <= 0)
+            {
+                return list;
+            }
+
+            list.Add(list[list.Count - 1] + list[list.Count - 2]);
+            return bySteps(list, count - 1);
+        }
+        public static List<int> byLimit(List<int> list, int limit)
+        {
+            int nextValue = list[list.Count - 1] + list[list.Count - 2];
+
+            if (nextValue > limit)
+            {
+                return list;
+            }
+
+            list.Add(nextValue);
+            return byLimit(list, limit);
+        }
+    }
+}
