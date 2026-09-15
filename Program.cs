2b6dec397088928abae91b8729c739e3b75678fc
TanyaBortnyk <tanabortnik374@gmail.com>
2026-09-15 21:47:36 +0300

    Lab1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Laboratorna1;
+class Program
+{
+    public static int[] readFromFile(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("файла не iснує");
+        }
+        string text = File.ReadAllText(fileName);
+        string[] parts = text.Split(',');
+        int[] number = new int[parts.Length];
+        for(int i = 0; i<parts.Length; i++)
+        {
+            number[i] = Convert.ToInt32(parts[i].Trim());
+        }
+        return number;
+
+    }
+     static void Main()
+    {
+        string input = "input.txt";
+        string steps = "steps.txt";
+        string limit = "limit.txt";
+        string result = "result.txt";
+        int[] inputNumbers = readFromFile(input);
+        int[] stepsNumbers = readFromFile(steps);
+        List<int> list = new List<int>(inputNumbers);
+        int countStep = stepsNumbers[0];
+        int[] limitNumbers = readFromFile(limit);
+        int countLimit = limitNumbers[0];
+        using (StreamWriter sw = new StreamWriter(result))
+
+        {
+            sw.WriteLine("Числа фiбоначчi для рекурсії: " + string.Join(", ", fibonacci.recursive(list, countStep)));
+            sw.Write("Числа фiбоначчi з лімітом: " + string.Join(", ", fibonacci.limitFibonacci(list, countLimit)));
+        }
+        Console.WriteLine("Данi записано у файл");
+     
+    }
+}
---
+++ fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorna1
+{
+     class fibonacci
+    {
+     public static List<int> recursive(List<int> list,int count)
+        {
+            if (count <= 2)
+            {
+                return list;
+            }
+            list.Add(list[list.Count-1] + list[list.Count-2]);
+            return recursive(list, count - 1);
+        }
+        public static List<int> limitFibonacci(List<int> list, int limit)
+        {
+            if (list.Count <= 2)
+            {
+                return list;
+            }
+            int nextValue = list[list.Count - 1] + list[list.Count - 2];
+            if (nextValue > limit)
+            {
+                return list;
+            }
+            list.Add(nextValue);
+            return limitFibonacci(list, limit);
+        }
+    }
+}
