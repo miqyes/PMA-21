81a7f3ab4583cbb184d13a88703732a30ee30979
irynaa <irynkabronich@gmail.com>
2026-09-16 22:51:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace UNI
+{
+    class Program
+    {
+        
+        
+        const string inpath = "input.txt";
+        const string steppath = "steps.txt";
+        
+        static void Main()
+        {
+            string inpath = "input.txt";
+            string steppath="steps.txt";
+            if (File.Exists(inpath)&& File.Exists(steppath))
+            {
+                Console.WriteLine("Files exist");
+            }
+            else
+            {
+                Console.WriteLine("No files exist");
+                return;
+            }
+
+            string[] values = File.ReadAllText(inpath).Trim().Split(',');
+            
+            string steps = File.ReadAllText(steppath).Trim();
+            int stepCount = int.Parse(steps);
+
+            var el = new List<int>();
+
+            el = Fibonacci.GetFib(el, stepCount);
+
+            string result = string.Join(",", el);
+            File.WriteAllText("output.txt", result);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO; 
+using System.Collections.Generic;
+
+namespace UNI
+{
+
+    public class Fibonacci
+    {
+        private const int limit = 100;
+
+        public static int GetLimit(int stepCount)
+        {
+            if (stepCount > limit)
+            {
+                throw new ArgumentException($"Amount of steps ({stepCount}) is greater {limit}");
+            }
+            return stepCount;
+        }
+        
+        
+        public static List<int> GetFib(List<int> val, int stepCount)
+        {
+            if (val.Count >= stepCount)
+            {
+                return val;
+            }
+            int newel = val[^1] + val[^2];
+            val.Add(newel);
+            return GetFib(val, stepCount);
+        }
+    }
+}
