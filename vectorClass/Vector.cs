0a6e1346b2aa6e95a18acc6bb229822f546f71e3
andrianochkaq <andriana.filatova2008@gmail.com>
2026-09-16 10:34:33 +0300

    refactoring(error handling)

--- vectorClass/Vector.cs
+++ vectorClass/Vector.cs
@@ -1,38 +1,81 @@
 namespace Task4;
-public class Vector {
-    double x{ get; }
-    double y { get; }
-    double z { get; }
-    public Vector(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z; }
 
-    public static Vector operator +(Vector vec1, Vector vec2) {
-        return new Vector(
-            vec1.x + vec2.x,
-            vec1.y + vec2.y,
-            vec1.z + vec2.z); }
-    
-    public static Vector operator -(Vector vec1, Vector vec2) {
-        return new Vector(
-            vec1.x - vec2.x,
-            vec1.y - vec2.y,
-            vec1.z - vec2.z); }
-    
-    public static Vector operator *(Vector vec, double number) {
-        return new Vector(
-            vec.x * number,
-            vec.y * number,
-            vec.z * number); }
-    
-    public static Vector operator /(Vector vec, double number) {
+public class Vector
+{
+    private double[] data;
+
+    public Vector(double[] data1)
+    {
+        data = data1;
+    }
+
+    public double this[int i]
+    {
+        get { return data[i]; }
+    }
+
+    public static Vector operator +(Vector vec1, Vector vec2)
+    {
+        if (vec1.data.Length != vec2.data.Length)
+            throw new Exception("Різні розміри");
+        double[] result = new double[vec1.data.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = vec1[i] + vec2[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public static Vector operator -(Vector vec1, Vector vec2)
+    {
+        if (vec1.data.Length != vec2.data.Length)
+            throw new Exception("Різні розміри");
+        double[] result = new double[vec1.data.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = vec1[i] - vec2[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public static Vector operator *(Vector vec, double number)
+    {
+        double[] result = new double[vec.data.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = vec[i] * number;
+        }
+
+        return new Vector(result);
+    }
+
+    public static Vector operator /(Vector vec, double number)
+    {
         if (number == 0)
-            throw new DivideByZeroException();
-        return new Vector(
-            vec.x/ number,
-            vec.y / number,
-            vec.z / number); }
-    
-    public override string ToString() {
-        return $"({x};{y};{z})"; } }
+            throw new Exception("Не можна ділити на 0(");
+
+        double[] result = new double[vec.data.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = vec[i] / number;
+        }
+
+        return new Vector(result);
+    }
+
+    public override string ToString()
+    {
+        string result = "(";
+        for (int i = 0; i < data.Length; i++)
+        {
+            result += data[i];
+            if (i < data.Length - 1)
+                result += ";";
+        }
+
+        result += ")"; 
+        return result;
+    }
+}
