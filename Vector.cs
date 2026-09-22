acbd32b44974a2e6c9206d58c83724b36eeae874
Мицько Ярина <yaryna.myt@gmail.com>
2026-09-22 23:54:07 +0300

    Add files via upload

---
+++ Vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace labVec
+{
+    class Vector
+    {
+        public double[] Coords;
+        public Vector(double[] coords)
+        {
+            Coords = coords;
+        }
+
+        public int GetSize()
+        {
+            return Coords.Length;
+        }
+        public bool hasZero()
+        {
+            for (int i = 0; i < Coords.Length; i++)
+            {
+                if (Coords[i] == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "(" + string.Join(", ", Coords) + ")";
+        }
+    }
+}
