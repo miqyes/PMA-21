d7614ebddb0e0b225162d06938f2a775e40fa03c
irynaa <irynkabronich@gmail.com>
2026-09-22 21:14:37 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,113 @@
+﻿namespace matrixclass
+{
+    public class Matrix
+    {
+        private double[,] a;
+        private double[,] b;
+        private int ra, ca;
+        private int rb, cb;
+
+        public Matrix(double[,] matra, double[,] matrb, int rowsa, int colsa, int rowsb, int colsb)
+        {
+            a = matra;
+            b = matrb;
+            ra=rowsa;
+            ca = colsa;
+            rb = rowsb;
+            cb = colsb;
+        }
+
+        public double[,] add()
+        {
+            if (b.GetLength(0) != a.GetLength(0) || b.GetLength(1) != a.GetLength(1))
+            {
+                throw new ArgumentException("Sizes are dif!Error!");
+            }
+
+            double[,] res = new double[ra, ca];
+            for (int i = 0; i < ra; i++)
+            {
+                for (int j = 0; j < ca; j++)
+                {
+                    res[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return res;
+        }
+
+        public double[,] sub()
+        {
+            if (b.GetLength(0) != a.GetLength(0) || b.GetLength(1) != a.GetLength(1))
+            {
+                throw new ArgumentException("Sizes are dif!Error!");
+            }
+
+            double[,] res = new double[ra, ca];
+            for (int i = 0; i < ra; i++)
+            {
+                for (int j = 0; j < ca; j++)
+                {
+                    res[i, j] = a[i, j] - b[i, j];
+                }
+            }
+
+            return res;
+        }
+
+        public double[,] mul()
+        {
+            double[,] res = new double[ra, cb];
+            for (int i = 0; i < ra; i++)
+            {
+                for (int j = 0; j < cb; j++)
+                {
+                    for (int k = 0; k < ca; k++)
+                    {
+                        res[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public double[,] div()
+        {
+            double[,] newb = new double[rb, cb];
+            for (int i = 0; i < rb; i++)
+            {
+                for (int j = 0; j < cb; j++)
+                {
+                    newb[cb - 1 - j, i] = b[i, j];
+                }
+            }
+
+            double[,] res = new double[ra, ca];
+            for (int i = 0; i < ra; i++)
+            {
+                for (int j = 0; j < ca; j++)
+                {
+                    for (int k = 0; k < ca; k++)
+                    {
+                        res[i, j] += a[i, k] * newb[k, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public void result(string act, double[,] matr)
+        {
+            string text = act + " =\n";
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                    text += matr[i, j] + " ";
+                text += "\n";
+            }
+
+            File.AppendAllText("outputmatrix.txt",text);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using matrixclass;
+
+namespace UNI
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string input = "inputmatrix.txt";
+            if (File.Exists(input))
+            {
+                Console.WriteLine("File exists");
+            }
+            else
+            {
+                Console.WriteLine("File does not exists");
+            }
+            
+            string[] lines = File.ReadAllLines(input);
+            int ra = 2;
+            int ca = 2;
+            double[,] a = new double[ra, ca];
+            for (int i = 0; i < ra; i++)
+            {
+                string[] row = lines[i].Split(' ');
+                for (int j = 0; j < ca; j++)
+                {
+                    a[i, j] = double.Parse(row[j]);
+                }
+            }
+            
+            int rb = 2;
+            int cb = 2;
+            double[,] b = new double[rb, cb];
+            for (int i = 0; i < rb; i++)
+            { 
+                string[] row = lines[ra + 1 + i].Split(' ');
+                for (int j = 0; j < cb; j++)
+                {
+                    b[i, j] = double.Parse(row[j]);
+                }
+            }
+
+            Matrix matrixres = new Matrix(a, b, ra, ca, rb, cb);
+            File.WriteAllText("outputmatrix.txt", string.Empty);
+            matrixres.result("A + B", matrixres.add());
+            matrixres.result("A - B", matrixres.sub());
+            matrixres.result("A * B", matrixres.mul());
+            matrixres.result("A / B", matrixres.div());
+        }
+    }
+}
