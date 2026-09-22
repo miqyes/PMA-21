1f42e25cd223fa325dd2c6b724964a9fcc3683aa
Мицько Ярина <yaryna.myt@gmail.com>
2026-09-22 23:44:30 +0300

    add lab3

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace vector
+{
+    class program
+    {
+        public static void Main()
+        {
+            if (!File.Exists("input.txt"))
+            {
+                Console.WriteLine("Error: file are not found!");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines("input.txt");
+            if (lines.Length < 2)
+            {
+                Console.WriteLine("Error: file must contain at least 2 lines!");
+                return;
+            }
+
+            string[] firstNumbers = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] secondNumbers = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (firstNumbers.Length != secondNumbers.Length)
+            {
+                Console.WriteLine("Error: both vectors must have the same number of components!");
+                return;
+            }
+
+            double[] arr1;
+            double[] arr2;
+            try
+            {
+                arr1 = firstNumbers.Select(double.Parse).ToArray();
+                arr2 = secondNumbers.Select(double.Parse).ToArray();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: file must contain only numbers!");
+                return;
+            }
+
+            vector v1 = new vector(arr1);
+            vector v2 = new vector(arr2);
+
+            vector sum = v1 + v2;
+            vector difference = v1 - v2;
+            vector product = v1 * v2;
+
+            // Перевірка на нуль тільки для ділення
+            bool hasZero = arr2.Any(c => c == 0);
+            string quotientLine;
+
+            if (hasZero)
+            {
+                quotientLine = "Quotient= " + v1 + "/" + v2 + "=ПОМИЛКА: ДІЛЕННЯ НА НУЛЬ НЕ МОЖЛИВЕ";
+            }
+            else
+            {
+                vector quotient = v1 / v2;
+                quotientLine = "Quotient=" + v1 + "/" + v2 + "=" + quotient;
+            }
+
+            string result = "First vector: " + v1 + Environment.NewLine +
+                "Second vector: " + v2 + Environment.NewLine +
+                "Sum=" + v1 + "+" + v2 + "=" + sum + Environment.NewLine +
+                "Difference=" + v1 + "-" + v2 + "=" + difference + Environment.NewLine +
+                "Product=" + v1 + "*" + v2 + "=" + product + Environment.NewLine +
+                quotientLine;
+
+            File.WriteAllText("result.txt", result);
+            Console.WriteLine("Result is successfully saved in file 'result.txt'");
+        }
+    }
+}
---
+++ vector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace vector
+{
+    public class vector
+    {
+        public double[] components;
+
+        public int Size => components.Length;
+
+        public vector(double[] components)
+        {
+            this.components = components;
+        }
+
+        public double this[int index]
+        {
+            get => components[index];
+            set => components[index] = value;
+        }
+
+        private static void CheckSameSize(vector a, vector b)
+        {
+            if (a.Size != b.Size)
+                throw new ArgumentException("Vectors must have the same dimension!");
+        }
+
+        public static vector operator +(vector a, vector b)
+        {
+            CheckSameSize(a, b);
+            double[] result = new double[a.Size];
+            for (int i = 0; i < a.Size; i++)
+                result[i] = a[i] + b[i];
+            return new vector(result);
+        }
+
+        public static vector operator -(vector a, vector b)
+        {
+            CheckSameSize(a, b);
+            double[] result = new double[a.Size];
+            for (int i = 0; i < a.Size; i++)
+                result[i] = a[i] - b[i];
+            return new vector(result);
+        }
+
+        public static vector operator *(vector a, vector b)
+        {
+            CheckSameSize(a, b);
+            double[] result = new double[a.Size];
+            for (int i = 0; i < a.Size; i++)
+                result[i] = a[i] * b[i];
+            return new vector(result);
+        }
+
+        public static vector operator /(vector a, vector b)
+        {
+            CheckSameSize(a, b);
+            double[] result = new double[a.Size];
+            for (int i = 0; i < a.Size; i++)
+                result[i] = a[i] / b[i];
+            return new vector(result);
+        }
+
+        public override string ToString()
+        {
+            return "(" + string.Join(",", components) + ")";
+        }
+    }
+}
